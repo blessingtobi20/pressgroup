8b70b125e673e4c573adb46552f3e3420a7adf37
blessingtobi20 <115149530+blessingtobi20@users.noreply.github.com>
2022-10-10 22:40:56 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,6 @@
+navbar = document.querySelector(".navbar").querySelectorAll("a");
+navbar.forEach(element => {
+    element.addEventListener("click", function () {
+        navbar.forEach(nav => nav.classList.remove("active"))
+    })
+})
